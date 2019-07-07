63adb9e41e4c72182c3f47b80aba87b9706af7d8
Juwami <sean.nguyenf@yahoo.com>
2019-07-07 00:17:03 -0400

    fix code, removed rl and added inquire. Had to add selectedWord into askLetter()

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 const Word = require('./Word.js')
-// const inquirer = require('inquirer')
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const inquirer = require('inquirer')
 
 const wordbank = ['dog', 'cat', 'fish', 'bird'];
 
@@ -13,28 +8,26 @@ function startGame() {
     const selectedWord = new Word();
     selectedWord.populateLetters(wordbank[randomIndex]);
     showPuzzle(selectedWord);
-    askLetter();
+    askLetter(selectedWord);
 }
 
-function askLetter() {
-    rl.question('Guess a letter... ', function (guess) {
-        selectedWord.guessLetter(guess);
-        showPuzzle(selectedWord);
-        // inquirer
-        //     .prompt([{
-        //         type: "input",
-        //         message: "What letter do you guess?",
-        //         name: "guess"
-        //     }])
-        //     .then(function (response) {
-        //         console.log(response.guess)
-        //         selectedWord.guessLetter(response.guess);
-        //         // showPuzzle();
-        //     })
-        // if they won, say you win, and run startGame()
-        // if haven't solved, run askLetter();
-        // can add askCount
-    });
+function askLetter(selectedWord) {
+    inquirer
+        .prompt([{
+            type: "input",
+            message: "What letter do you guess?",
+            name: "guess"
+        }])
+        .then(function (response) {
+            console.log(response.guess)
+            selectedWord.guessLetter(response.guess);
+            showPuzzle(selectedWord);
+            askLetter(selectedWord)
+        })
+    // if they won, say you win, and run startGame()
+    // if haven't solved, run askLetter();
+    // can add askCount
+    // });
 }
 
 function showPuzzle(word) {
