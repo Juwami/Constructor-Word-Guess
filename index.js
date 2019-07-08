5e7dda0d37af4e6d8a0a10834ebc51d8abe5babd
Juwami <sean.nguyenf@yahoo.com>
2019-07-07 22:33:24 -0400

    added remaining count prompt and end game

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Word = require('./Word.js')
 const inquirer = require('inquirer')
 
-const wordbank = ['dog', 'cat', 'fish', 'bird'];
+const wordbank = ['Complete','Blanket','Tower','Doorbell','Brothers','Party','Wagon','Money','Vacation','Computer','Wheelchair','Fingernail','Vegetable','Television'];
+
+let remainingGuesses = 2
 
 function startGame() {
     const randomIndex = Math.floor(Math.random() * wordbank.length);
@@ -12,6 +14,7 @@ function startGame() {
 }
 
 function askLetter(selectedWord) {
+    if (remainingGuesses > 0) {
     inquirer
         .prompt([{
             type: "input",
@@ -19,15 +22,33 @@ function askLetter(selectedWord) {
             name: "guess"
         }])
         .then(function (response) {
-            console.log(response.guess)
+            remainingGuesses--
+            console.log(`You have ${remainingGuesses} guesses left.`)
+            // console.log(response.guess)
             selectedWord.guessLetter(response.guess);
             showPuzzle(selectedWord);
             askLetter(selectedWord)
         })
+    }
+    else {
+        console.log('You have lost!')
+        inquirer
+        .prompt([{
+            type: "list",
+            name: "endgame",
+            message: "Do you want to start over?",
+            choices: ['Yes','No']
+        }])
+        .then(function (response) {
+            console.log(response)
+            // if (response.yes) {
+            //     startGame();
+            // }
+        })
+    }
     // if they won, say you win, and run startGame()
     // if haven't solved, run askLetter();
     // can add askCount
-    // });
 }
 
 function showPuzzle(word) {
